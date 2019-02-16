c4e4e05e735836a4cbeae5a75f3873867e94193d
dsekerov <darijan2002@gmail.com>
2019-02-16 01:12:23 +0100

    added Ucenik and Smer models started Requests implementation

---
+++ client/Middleware/Models.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Middleware
+{
+    public class Ucenik
+    {
+        [JsonProperty("ime")]
+        public string _ime { get; set; }
+        [JsonProperty("tatkovo")]
+        public string _tatkovo { get; set; }
+        [JsonProperty("prezime")]
+        public string _prezime { get; set; }
+        [JsonProperty("oceni")]
+        public List<int> _oceni { get; set; }
+        [JsonProperty("paralelka")]
+        public string _paralelka { get; set; }
+        [JsonProperty("smer")]
+        public string _smer { get; set; }
+
+        public Ucenik()
+        {
+            _ime = "";
+            _tatkovo = "";
+            _prezime = "";
+            _oceni = new List<int>();
+            _paralelka = "";
+            _smer = "";
+        }
+
+        public string Serialize()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+    }
+
+    static public class Smerovi
+    {
+        public const string PMA = "PMA";
+        public const string PMB = "PMB";
+        public const string OHA = "OHA";
+        public const string OHB = "OHB";
+        public const string JUA = "JUA";
+        public const string JUB = "JUB";
+
+    }
+
+    public class Smer
+    {
+        [JsonProperty("predmeti")]
+        public List<string> _predmeti { get; set; }
+
+        public Smer()
+        {
+            _predmeti = new List<string>();
+        }
+
+        public string Serialize()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+    }
+}
---
+++ client/Middleware/Requests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Middleware
+{
+    public class Requests
+    {
+        private static HttpClient http = new HttpClient();
+        public static string DodajUcenik(Ucenik u)
+        {
+            string json = u.Serialize();
+            Dictionary<string, string> request = new Dictionary<string, string>()
+            {
+                {"action", "dodaj" },
+                {"ucenik", json }
+            };
+
+            //string uri = string.Format(@"http://{0}:{1}/api", settings.Default.DB_HOST, settings.Default.DB_PORT);
+            //var req = (HttpWebRequest)WebRequest.Create(uri);
+            //req.Method = "POST";
+            //req.ContentType = "application/json";
+
+            //var response = await http.PostAsync(uri, new FormUrlEncodedContent(request));
+            //return await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.SerializeObject(request);
+        }
+    }
+}
