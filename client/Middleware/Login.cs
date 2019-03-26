fddc5299e4a2034d093e63466d190b56939b0cdf
svaic <47664495+svaic@users.noreply.github.com>
2019-03-26 22:56:45 +0100

    Update Login.cs

--- client/Middleware/Login.cs
+++ client/Middleware/Login.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
+using System.Threading.Tasks;;
 
 namespace Middleware
 {
