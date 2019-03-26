e90d180f611d6a161abe57f79d5b0fb337f28f89
Luka Jovanovski <luka.jovanovski@hotmail.com>
2019-03-26 23:02:45 +0100

    1.0

--- client/Frontend/Data.cs
+++ client/Frontend/Data.cs
@@ -23,7 +23,7 @@ namespace MainWindows
             {
                 if(Users[i,0] == Username && Users[i,1] == Password)
                 {
-                    return i +1;
+                    return i;
                 }
             }
 
