7e46404ea9e0c5ac8e6a9fc5236d3889b69767bf
dsekerov <darijan2002@gmail.com>
2019-04-22 04:02:11 +0200

    pocnav so print za glavna kniga

--- client/Middleware/Print.cs
+++ client/Middleware/Print.cs
@@ -13,7 +13,7 @@ namespace Middleware
     {
         public static void PrintSveditelstvo(List<Ucenik> ucenici, Klasen klasen, int printerChoice)
         {
-            List<string> data = init(ucenici, klasen);
+            List<string> data = InitSveditelstvo(ucenici, klasen);
             string rootFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
             PrintDialog printDialog = new PrintDialog();
             PrintDocument pd = new PrintDocument();
@@ -24,20 +24,15 @@ namespace Middleware
             pd.DefaultPageSettings.PaperSize = pd.PrinterSettings.PaperSizes.Cast<PaperSize>().First<PaperSize>(size => size.Kind == PaperKind.A4);
             pd.OriginAtMargins = false;
 
-            /*List<string> data = new List<string>() {
-            //    "\"Македонски Јазик/ Математичка Анализа / Бизнис и Претприемништво/ ФЗО / Германски Јазик / Програмски Јазици/ Predmet123/ Predmet321/ Predmet221/ Predmet 231\";\"1 2 3 4 5 2 2 3 5 4\";\"Коце Металец, Скопје,,IV, Лука Јовановски,Славка Јовановска,22.09.2000,Скопје,Скопје,Северна Македонија, северно-македонец,19,20,прв,IV 1,Добро,0,0,Гимназиско образование,ПМА,,\"",
-            //    "\"makedonski jazik/ calculus IV/ Бизнис и Претприемништво/ ФЗО / Германски Јазик / Програмски Јазици/ Predmet123/ Predmet321/ Predmet221/ Predmet 231\";\"1 2 3 4 5 2 2 3 5 4\";\"Коце Металец, Скопје,,IV, Лука Јовановски,Славка Јовановска,22.09.2000,Скопје,Скопје,Северна Македонија, северно-македонец,19,20,прв,IV 1,Добро,0,0,Гимназиско образование,ПМА,,\"",
-            //     "\"SOCIOLOGIJAAA/ Математичка Анализа / Бизнис и Претприемништво/ ФЗО / Германски Јазик / Програмски Јазици/ Predmet123/ Predmet321/ Predmet221/ Predmet 231\";\"1 2 3 4 5 2 2 3 5 4\";\"Коце Металец, Скопје,,IV, Лука Јовановски,Славка Јовановска,22.09.2000,Скопје,Скопје,Северна Македонија, северно-македонец,19,20,прв,IV 1,Добро,0,0,Гимназиско образование,ПМА,,\""
-            //};
-            */
+            data.Insert(0, "sveditelstvo"); // mozno e da e "sveditelstva"
             string outparam = String.Join("?", data);
 
-            string pyscript = rootFolder + "\\main.exe";
+            string pyscript = rootFolder + "\\print.exe";
             Process py = new Process();
             py.StartInfo.FileName = new Uri(pyscript).AbsolutePath;
             py.StartInfo.UseShellExecute = false;
-            py.StartInfo.RedirectStandardInput = true;
             py.StartInfo.Arguments = outparam;
+            py.StartInfo.CreateNoWindow = true;
             py.Start();
             py.WaitForExit();
 
@@ -45,8 +40,6 @@ namespace Middleware
             {
                 pd.PrinterSettings.Duplex = Duplex.Vertical;
             }
-
-            //py.StartInfo.CreateNoWindow = true;
 
             for (int i = 0; i < data.Count; i++)
             {
@@ -88,7 +81,229 @@ namespace Middleware
                 }
             }
         }
-        public static List<string> init(List<Ucenik> ucenici, Klasen klasen)
+        public static List<string> InitSveditelstvo(List<Ucenik> ucenici, Klasen klasen)
+        {
+            StringWriter sw = new StringWriter();
+            List<string> l = new List<string>();
+            string delimiter = ",";
+            foreach (Ucenik u in ucenici)
+            {
+                sw.GetStringBuilder().Clear();
+
+                // predmeti
+                sw.Write("\"" + String.Join("/", u._s._predmeti) + "\"");
+                sw.Write(";");
+
+                // oceni
+                sw.Write("\"" + String.Join(" ", u._oceni) + "\"");
+                sw.Write(";");
+
+                // uchilishte, grad, broj glavna kniga, godina (klas)
+                sw.Write("\"");
+                sw.Write(klasen._uchilishte);
+                sw.Write(delimiter);
+                sw.Write(klasen._grad);
+                sw.Write(delimiter);
+                sw.Write(delimiter); // broj glavna kniga
+                sw.Write(klasen._paralelka.Split('-').FirstOrDefault());
+                sw.Write(delimiter);
+
+                // ime prezime na ucenik, ime prezime na roditel, DOB, naselba, opshtina, drzhava, drzhavjanstvo (hardcode)
+                sw.Write(u._ime + " " + u._prezime);
+                sw.Write(delimiter);
+                sw.Write(u._tatkovo + " " + u._prezime);
+                sw.Write(delimiter);
+                sw.Write(u._roden);
+                sw.Write(delimiter);
+                sw.Write(u._mesto);
+                sw.Write(delimiter);
+                sw.Write("Македонец"); // hardcoded drzavjanstvo
+                sw.Write(delimiter);
+
+                // momentalna i sledna ucebna godina, po koj pat ja uci godinata
+                sw.Write(klasen._godina.ToString());
+                sw.Write(delimiter);
+                sw.Write((klasen._godina + 1).ToString());
+                sw.Write(delimiter);
+                sw.Write(u._pat.ToString());
+                sw.Write(delimiter);
+
+                // paralelka, povedenie, opravdani, neopravdani, tip, smer
+                sw.Write(klasen._paralelka);
+                sw.Write(delimiter);
+                sw.Write(u._povedenie);
+                sw.Write(delimiter);
+                sw.Write(u._opravdani.ToString());
+                sw.Write(delimiter);
+                sw.Write(u._neopravdani.ToString());
+                sw.Write(delimiter);
+                sw.Write(u._tip);
+                sw.Write(delimiter);
+                sw.Write(u._smer);
+                sw.Write(delimiter);
+
+                // XX, YY
+                sw.Write(delimiter); // XX
+                sw.Write(""); // YY
+
+                sw.Write("\"");
+
+                l.Add(sw.ToString());
+            }
+            return l;
+        }
+        public static void PrintGlavnaKniga(List<Ucenik> ucenici, Klasen klasen, int printerChoice)
+        {
+            List<string> data = InitGlavnaKniga(ucenici, klasen);
+            string rootFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            PrintDialog printDialog = new PrintDialog();
+            PrintDocument pd = new PrintDocument();
+
+            // PrinterSettings.InstalledPrinters - lista na printeri
+            pd.PrinterSettings.PrinterName = PrinterSettings.InstalledPrinters[printerChoice];
+
+            pd.DefaultPageSettings.PaperSize = pd.PrinterSettings.PaperSizes.Cast<PaperSize>().First<PaperSize>(size => size.Kind == PaperKind.A3);
+            pd.OriginAtMargins = false;
+            pd.DefaultPageSettings.Landscape = false;
+
+            data.Insert(0, "glavna"); // mozno e da e "sveditelstva"
+            string outparam = String.Join("?", data);
+
+            string pyscript = rootFolder + "\\print.exe";
+            Process py = new Process();
+            py.StartInfo.FileName = new Uri(pyscript).AbsolutePath;
+            py.StartInfo.UseShellExecute = false;
+            py.StartInfo.Arguments = outparam;
+            py.StartInfo.CreateNoWindow = true;
+            py.Start();
+            py.WaitForExit();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                pd.PrintPage += (sender, args) =>
+                {
+                    args.Graphics.DrawImage(System.Drawing.Image.FromFile(String.Format(".\\test-{0}.jpg", i)), args.PageBounds);
+                    pd.DocumentName = String.Format("{0}\\test-{1}.jpg", rootFolder, i);
+                };
+                pd.Print();
+            }
+        }
+        public static List<string> InitGlavnaKniga(List<Ucenik> ucenici, Klasen klasen)
+        {
+            StringWriter sw = new StringWriter();
+            List<string> l = new List<string>();
+            string delimiter = ",";
+            foreach (Ucenik u in ucenici)
+            {
+                sw.GetStringBuilder().Clear();
+                List<string> tmparr = new List<string>();
+
+                // predmeti
+                tmparr.Clear();
+                tmparr.AddRange(u._s._predmeti);
+                while(tmparr.Count < 17)
+                {
+                    tmparr.Add("NaN");
+                }
+                sw.Write("\"" + String.Join("/", u._s._predmeti) + "\"");
+                sw.Write(";");
+
+                // oceni
+                sw.Write("\"" + String.Join(" ", u._oceni) + "\"");
+                sw.Write(";");
+
+                // uchilishte, grad, broj glavna kniga, godina (klas)
+                sw.Write("\"");
+                sw.Write(klasen._uchilishte);
+                sw.Write(delimiter);
+                sw.Write(klasen._grad);
+                sw.Write(delimiter);
+                sw.Write(delimiter); // broj glavna kniga
+                sw.Write(klasen._paralelka.Split('-').FirstOrDefault());
+                sw.Write(delimiter);
+
+                // ime prezime na ucenik, ime prezime na roditel, DOB, naselba, opshtina, drzhava, drzhavjanstvo (hardcode)
+                sw.Write(u._ime + " " + u._prezime);
+                sw.Write(delimiter);
+                sw.Write(u._tatkovo + " " + u._prezime);
+                sw.Write(delimiter);
+                sw.Write(u._roden);
+                sw.Write(delimiter);
+                sw.Write(u._mesto);
+                sw.Write(delimiter);
+                sw.Write("Македонец"); // hardcoded drzavjanstvo
+                sw.Write(delimiter);
+
+                // momentalna i sledna ucebna godina, po koj pat ja uci godinata
+                sw.Write(klasen._godina.ToString());
+                sw.Write(delimiter);
+                sw.Write((klasen._godina + 1).ToString());
+                sw.Write(delimiter);
+                sw.Write(u._pat.ToString());
+                sw.Write(delimiter);
+
+                // paralelka, povedenie, opravdani, neopravdani, tip, smer
+                sw.Write(klasen._paralelka);
+                sw.Write(delimiter);
+                sw.Write(u._povedenie);
+                sw.Write(delimiter);
+                sw.Write(u._opravdani.ToString());
+                sw.Write(delimiter);
+                sw.Write(u._neopravdani.ToString());
+                sw.Write(delimiter);
+                sw.Write(u._tip);
+                sw.Write(delimiter);
+                sw.Write(u._smer);
+                sw.Write(delimiter);
+
+                // XX, YY
+                sw.Write(delimiter); // XX
+                sw.Write(""); // YY
+
+                sw.Write("\"");
+
+                l.Add(sw.ToString());
+            }
+            return l;
+        }
+        public static void PrintDiploma(List<Ucenik> ucenici, Klasen klasen, int printerChoice)
+        {
+            List<string> data = InitDiploma(ucenici, klasen);
+            string rootFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            PrintDialog printDialog = new PrintDialog();
+            PrintDocument pd = new PrintDocument();
+
+            // PrinterSettings.InstalledPrinters - lista na printeri
+            pd.PrinterSettings.PrinterName = PrinterSettings.InstalledPrinters[printerChoice];
+
+            pd.DefaultPageSettings.PaperSize = pd.PrinterSettings.PaperSizes.Cast<PaperSize>().First<PaperSize>(size => size.Kind == PaperKind.A3);
+            pd.OriginAtMargins = false;
+            pd.DefaultPageSettings.Landscape = false;
+            pd.DefaultPageSettings.Landscape = true;
+
+            data.Insert(0, "dipl"); // mozno e da e "sveditelstva"
+            string outparam = String.Join("?", data);
+
+            string pyscript = rootFolder + "\\print.exe";
+            Process py = new Process();
+            py.StartInfo.FileName = new Uri(pyscript).AbsolutePath;
+            py.StartInfo.UseShellExecute = false;
+            py.StartInfo.Arguments = outparam;
+            py.StartInfo.CreateNoWindow = true;
+            py.Start();
+            py.WaitForExit();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                pd.PrintPage += (sender, args) =>
+                {
+                    args.Graphics.DrawImage(System.Drawing.Image.FromFile(String.Format(".\\dipl-{0}.jpg", i)), args.PageBounds);
+                    pd.DocumentName = String.Format("{0}\\dipl-{1}.jpg", rootFolder, i);
+                };
+                pd.Print();
+            }
+        }
+        public static List<string> InitDiploma(List<Ucenik> ucenici, Klasen klasen)
         {
             StringWriter sw = new StringWriter();
             List<string> l = new List<string>();
