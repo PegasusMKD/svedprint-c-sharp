69ab48dd817115cd6659e355647861e0812653de
Luka Jovanovski <luka.jovanovski@hotmail.com>
2019-06-15 14:24:35 +0200

    remove hardcoded login

--- client/Frontend/Login_Page.xaml.cs
+++ client/Frontend/Login_Page.xaml.cs
@@ -36,8 +36,7 @@ namespace Frontend
         
         private void Login_Btn_Click(object sender, RoutedEventArgs e)
         {
-            Username_txt.Text = "Владица_Василевска";
-            Password_txt.Text = "vl@d@89";
+            
             login();
         }
 
