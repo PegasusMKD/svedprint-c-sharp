e3ec4b1358127a570d0212e364e4ebc135aaa8d7
Luka Jovanovski <luka.jovanovski@hotmail.com>
2019-06-15 17:06:27 +0200

    sort fixed

--- client/Frontend/EditUcenici_Page.xaml.cs
+++ client/Frontend/EditUcenici_Page.xaml.cs
@@ -47,127 +47,11 @@ namespace Frontend
             UserKlas = Home_Page.KlasenKlasa;
             Ucenici = Home_Page.ucenici;
             SortUcenici();
-            //GetData();
             Refresh();
-        }
-
-        List<TextBox> Answer;
-        List<ComboBox> CBList;
-        private void GetData5()
-        { 
-            /*
-            Answer = new List<TextBox>();
-            CBList = new List<ComboBox>();
-            MainGrid.Children.Clear();
-            Dictionary<string, string> dictionary = new Dictionary<string, string>();
-            dictionary.Add("Име", "Име");
-            dictionary.Add("Презиме", "Презиме");
-            dictionary.Add("Средно Име", "Средно Име");
-            dictionary.Add("Смер", "Смер");
-            dictionary.Add("родител(Татко)", "Име Презиме");
-            dictionary.Add("родител(Мајка)", "Име Презиме");
-            dictionary.Add("број во дневник", "00");
-            dictionary.Add("Државјанство", "Македонско");
-            dictionary.Add("Пол", "Машки/Женски");
-            dictionary.Add("ден на раѓање", "00.00.0000");
-            dictionary.Add("место на раѓање", "Скопје");
-            dictionary.Add("место на живеење", "Скопје");
-            dictionary.Add("по кој пат ја учи годината", "прв/втор пат");
-            dictionary.Add("дали е положена годината", "Положил");
-            dictionary.Add("број на оправдани изостаноци", "0");
-            dictionary.Add("број на неоправдани изостаноци", "0");
-            dictionary.Add("Поведение", "Примeрно");
-
-            if (Ucenici.Count > BrojDn)
-            {
-                dictionary["Име"] = Ucenici[BrojDn]._ime;
-                dictionary["Презиме"] = Ucenici[BrojDn]._prezime;
-                dictionary["Средно Име"] = Ucenici[BrojDn]._srednoIme;
-                dictionary["Смер"] = Ucenici[BrojDn]._smer;
-                Ucenici[BrojDn]._broj = BrojDn;
-                dictionary["број во дневник"] = (Ucenici[BrojDn]._broj + 1).ToString();
-                dictionary["родител(Татко)"] = Ucenici[BrojDn]._tatko;
-                dictionary["родител(Мајка)"] = Ucenici[BrojDn]._majka;
-                dictionary["Државјанство"] = Ucenici[BrojDn]._drzavjanstvo;
-                dictionary["Пол"] = Ucenici[BrojDn]._gender;
-                dictionary["ден на раѓање"] = Ucenici[BrojDn]._roden;
-                dictionary["место на раѓање"] = Ucenici[BrojDn]._mesto_na_ragjanje;
-                dictionary["место на живеење"] = Ucenici[BrojDn]._mesto_na_zhiveenje;
-                dictionary["по кој пат ја учи годината"] = Ucenici[BrojDn]._pat_polaga;
-                dictionary["дали е положена годината"] = Ucenici[BrojDn]._polozhil;
-                dictionary["број на оправдани изостаноци"] = Ucenici[BrojDn]._opravdani.ToString();
-                dictionary["број на неоправдани изостаноци"] = Ucenici[BrojDn]._neopravdani.ToString();
-                dictionary["Поведение"] = Ucenici[BrojDn]._povedenie;
-            }
-            MainGrid.Height = 0.0;
-            int index = 0;
-            foreach (KeyValuePair<string, string> keyValuePair in dictionary)
-            {
-                StackPanel stackPanel = new StackPanel();
-                if (index % 2 == 0)
-                {
-                    MainGrid.RowDefinitions.Add(new RowDefinition());
-                    MainGrid.RowDefinitions[MainGrid.RowDefinitions.Count - 1].Height = new GridLength(100.0);
-                    MainGrid.Height += 100.0;
-                }
-                TextBox textBox = ContentTextBox(keyValuePair.Value);
-                textBox.Name = names[keyValuePair.Key];
-                Answer.Add(textBox);
-                stackPanel.Children.Add(ContentBorder(keyValuePair.Key));
-                stackPanel.Children.Add(Answer[index]);
-                stackPanel.Children.Add(UnderTextBorder());
-                Grid.SetRow(stackPanel, index / 2);
-                Grid.SetColumn(stackPanel, index % 2);
-                MainGrid.Children.Add(stackPanel);
-                ++index;
-            }
-
-            string[][] cb = new string[][] { new string[] { "Проектни Активности", "proektni", "izboren1_odg", }, new string[] { "Проектни Активности", "proektni", "izboren2_odg" } };
-            foreach (string[] x in cb)
-            {
-                StackPanel stackPanel = new StackPanel();
-                if (index % 2 == 0)
-                {
-                    MainGrid.RowDefinitions.Add(new RowDefinition());
-                    MainGrid.RowDefinitions[MainGrid.RowDefinitions.Count - 1].Height = new GridLength(100.0);
-                    MainGrid.Height += 130.0;
-                }
-
-                ComboBox CB = new ComboBox();
-                CB.Tag = x[1];
-                CB.Margin = new Thickness(30, 5, 30, 5);
-                if (UserKlas._p._smerovi.ContainsKey("ПА"))
-                {
-                    CB.ItemsSource = UserKlas._p._smerovi["ПА"]._predmeti;
-                    CB.SelectedIndex = 0;
-                }
-                CBList.Add(CB);
-
-                stackPanel.Children.Add(ContentBorder(x[0]));
-                stackPanel.Children.Add(CBList.Last());
-
-                string[] odgovori = { "Реализирал", "Не Реализирал" };
-                ComboBox res = new ComboBox();
-                res.Tag = x[2];
-                res.Margin = new Thickness(30, 5, 30, 5);
-                res.ItemsSource = odgovori;
-                CBList.Add(res);
-                res.SelectedIndex = 0;
-
-                stackPanel.Children.Add(CBList.Last());
-                stackPanel.Children.Add(UnderTextBorder());
-
-                Grid.SetRow(stackPanel, index / 2);
-                Grid.SetColumn(stackPanel, index % 2);
-                MainGrid.Children.Add(stackPanel);
-
-                ++index;
-            }*/
         }
 
         private void ContentTextBoxLostFocus(object sender, RoutedEventArgs e)
         {
-            // changes[((TextBox)sender).Name] = ((TextBox)sender).Text;
         }
 
         List<TextBox> ListPolinjaTxt;
@@ -177,6 +61,9 @@ namespace Frontend
         {
             ListPolinjaTxt = new List<TextBox>();
             ListPolinjaCB = new List<ComboBox>();
+            st1.Children.Clear();
+            st2.Children.Clear();
+            polinja.Clear();
             string[] PApredmeti = { "Ги немате пополнето Проектните Активности" };
             string[] Smerovi = { "Ги немате пополнето Смеровите" };
             if (UserKlas._p._smerovi.Keys.Contains("ПА")) PApredmeti = UserKlas._p._smerovi["ПА"]._predmeti.ToArray();
@@ -191,7 +78,7 @@ namespace Frontend
             for(int i=0;i<19;i++)
             { Saved.Add(""); }
 
-            if(Ucenici.Count > 0)
+            if(Ucenici.Count > 0 && Ucenici.Count > BrojDn)
             {
                 Saved[0]= Ucenici[BrojDn]._ime;
                 Saved[1] = Ucenici[BrojDn]._prezime;
@@ -213,14 +100,15 @@ namespace Frontend
                 Saved[17] = Ucenici[BrojDn]._proektni.Split(';')[0]; // " , ;"
                 Saved[18] = Ucenici[BrojDn]._proektni.Split(';')[1];
             }
+            if (Saved[6] == "") Saved[6] = (BrojDn+1).ToString();
 
             polinja.Add(new Pole("Име", RequestParameters.ime,  new string[] { "Име" } , Saved[0]) );
-            polinja.Add(new Pole("Презиме", RequestParameters.ime, new string[] { "Презиме" }, Saved[1]));
+            polinja.Add(new Pole("Презиме", RequestParameters.prezime, new string[] { "Презиме" }, Saved[1]));
             polinja.Add(new Pole("Татково име", RequestParameters.srednoIme, new string[] { "Татково име" }, Saved[2]));
             polinja.Add(new Pole("Смер", RequestParameters.smer, Smerovi, Saved[3]));
-            polinja.Add(new Pole("Родител(Татко)", RequestParameters.majka, new string[] { "Име Презиме" }, Saved[4]));
-            polinja.Add(new Pole("Родител(Мајка)", RequestParameters.tatko, new string[] { "Име Презиме" }, Saved[5]));
-            polinja.Add(new Pole("број во дневник", RequestParameters.broj, new string[] { "00" }, Saved[6]));
+            polinja.Add(new Pole("Родител(Татко)", RequestParameters.tatko, new string[] { "Име Презиме" }, Saved[4]));
+            polinja.Add(new Pole("Родител(Мајка)", RequestParameters.majka, new string[] { "Име Презиме" }, Saved[5]));
+            polinja.Add(new Pole("број во дневник", RequestParameters.broj, new string[] { "0" }, Saved[6]));
             polinja.Add(new Pole("Државјанство", RequestParameters.drzavjanstvo, new string[] { "Македонско" }, Saved[7]));
             polinja.Add(new Pole("Пол", RequestParameters.gender, new string[] { "Машки" , "Женски" }, Saved[8]));
             polinja.Add(new Pole("ден на раѓање", RequestParameters.roden, new string[] { "00.00.0000" }, Saved[9]));
@@ -231,25 +119,20 @@ namespace Frontend
             polinja.Add(new Pole("број на оправдани изостаноци", RequestParameters.opravdani, new string[] { "0" }, Saved[14]));
             polinja.Add(new Pole("број на неоправдани изостаноци", RequestParameters.neopravdani, new string[] { "0" }, Saved[15]));
             polinja.Add(new Pole("Поведение", RequestParameters.povedenie, new string[] { "Примeрно" , "Добро" , "Незадоволно" }, Saved[16]));
-            polinja.Add(new Pole("Проектна Активност 1", RequestParameters.povedenie, PApredmeti, Saved[17]));
-            polinja.Add(new Pole("Проектна Активност 2", RequestParameters.povedenie, PApredmeti, Saved[18]));
+            polinja.Add(new Pole("Проектна Активност 1", RequestParameters.proektni, PApredmeti, Saved[17]));
+            polinja.Add(new Pole("Проектна Активност 2", RequestParameters.proektni, PApredmeti, Saved[18]));
 
             int index = 0;
             foreach (Pole Pole in polinja)
             {
                 StackPanel stackPanel = new StackPanel();
-                if (index % 2 == 0)
-                {
-                    MainGrid.RowDefinitions.Add(new RowDefinition());
-                    MainGrid.RowDefinitions[MainGrid.RowDefinitions.Count - 1].Height = new GridLength(100.0);
-                    MainGrid.Height += 100.0;
-                }
 
                 StackPanel st = new StackPanel();
 
                 if (Pole.isCB)
                 {
                     ComboBox CB = CreateComboBox(Pole.Ime , Pole.DefaultVrednost);
+                    CB.SelectionChanged += CB_SelectionChanged;
                     if(Pole.Odgovor == "")CB.SelectedIndex = 0;
                     else CB.SelectedValue = Pole.Odgovor;
                     ListPolinjaCB.Add(CB);
@@ -258,6 +141,7 @@ namespace Frontend
                     {
                         ComboBox CBPoleodg = CreateComboBox(Pole.Ime + "ODG", new string[] { "Реализирал", "Не Реализирал" });
                         CBPoleodg.SelectedIndex = 0;
+                        CBPoleodg.Tag = RequestParameters.proektni;
                         ListPolinjaCB.Add(CBPoleodg);
                         st.Children.Add(CBPoleodg);
                     }
@@ -266,6 +150,8 @@ namespace Frontend
                 {
                     TextBox textBox = ContentTextBox(Pole.GetOdgovor());
                     textBox.GotFocus += txtPolinjaGotFocus;
+                    textBox.LostFocus += txtPolinjaLostFocus;
+                    textBox.TextChanged += txtPolinjaTxtChanged;
                     textBox.Tag = Pole.Ime;
                     ListPolinjaTxt.Add(textBox);
                     st.Children.Add(textBox);
@@ -274,11 +160,48 @@ namespace Frontend
                 stackPanel.Children.Add(ContentBorder(Pole.Ime));
                 stackPanel.Children.Add(st);
                 stackPanel.Children.Add(UnderTextBorder());
+                /*
                 Grid.SetRow(stackPanel, index / 2);
                 Grid.SetColumn(stackPanel, index % 2);
                 MainGrid.Children.Add(stackPanel);
+                if (index % 2 == 0)
+                {
+                    MainGrid.RowDefinitions.Add(new RowDefinition());
+                    MainGrid.RowDefinitions[MainGrid.RowDefinitions.Count - 1].Height = new GridLength();
+                    MainGrid.Height += stackPanel.ActualHeight;
+                }
+                */
+                if (index % 2 == 0)
+                {
+                    st1.Children.Add(stackPanel);
+                }
+                else
+                {
+                    st2.Children.Add(stackPanel);
+                }
                 ++index;
             }
+        }
+
+        private void txtPolinjaTxtChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tx = (TextBox)(sender);
+            Pole pol = polinja.Find(x => x.Ime == tx.Tag.ToString());
+            if(tx.Text != "")polinja.Find(x => x.Ime == tx.Tag.ToString()).Odgovor = tx.Text;
+        }
+
+        private void CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)(sender);
+            polinja.Find(x => x.Ime == cb.Tag.ToString()).Odgovor = cb.SelectedValue.ToString();
+        }
+
+        private void txtPolinjaLostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tx = (TextBox)(sender);
+            Pole pol = polinja.Find(x => x.Ime == tx.Tag.ToString());
+            if (tx.Text == "") AddText(sender, pol.DefaultVrednost[0]);
+            else polinja.Find( x=> x.Ime == tx.Tag.ToString()).Odgovor = tx.Text;
         }
 
         private void txtPolinjaGotFocus(object sender, RoutedEventArgs e)
@@ -322,7 +245,7 @@ namespace Frontend
 
         String Valid(int x)
         {
-            if (BrojDn + x >= 0 && BrojDn + x < Ucenici.Count) BrojDn += x;
+            if (BrojDn + x >= 0 && BrojDn + x <= Ucenici.Count) BrojDn += x;
             return (BrojDn + 1).ToString();
         }
 
@@ -332,7 +255,7 @@ namespace Frontend
             {
                 MessageBox.Show("Ученикот со таков број во дневник не постои");
             }
-            Save();
+            Save2();
         }
 
         private void Save()
@@ -347,6 +270,62 @@ namespace Frontend
             OrigData["broj"] = BrojDn.ToString();
             MessageBox.Show(Output(Ucenici[BrojDn].UpdateUcenikData(tx, OrigData, UserKlas._token)));
             SortUcenici();
+        }
+
+        private void Save2()
+        {
+            Dictionary<string, string> tx = new Dictionary<string, string>();
+            Dictionary<string, string> OrigData = new Dictionary<string, string>();
+            List<string> proektni = new List<string>();
+            foreach (Pole x in polinja)
+            {
+                if (x.RequestParametar != RequestParameters.proektni) tx.Add(x.RequestParametar, x.GetOdgovor());
+                else
+                {
+                    proektni.Add(x.GetOdgovor());
+                    proektni.Add(ListPolinjaCB.Find(y => y.Tag.ToString() == x.RequestParametar).SelectedValue.ToString());
+                }
+                if(x.RequestParametar == RequestParameters.broj)
+                {
+                    if(int.Parse(x.GetOdgovor()) != BrojDn+1)
+                    {
+                        
+                        int i = int.Parse(x.GetOdgovor());
+                        Ucenici[i-1]._broj = BrojDn+1;
+                        Ucenici[i - 1].UpdateUcenik(RequestParameters.broj, (BrojDn + 1).ToString(),UserKlas._token);
+                        Ucenici[BrojDn]._broj = i;
+                    }
+                }
+            }
+            if(proektni.Count > 0)
+            {
+                tx.Add(RequestParameters.proektni, ProektniToString(proektni));
+            }
+
+            OrigData["ime"] = Ucenici[BrojDn]._ime;
+            OrigData["prezime"] = Ucenici[BrojDn]._prezime;
+            OrigData["srednoIme"] = Ucenici[BrojDn]._srednoIme;
+            OrigData["broj"] = BrojDn.ToString();
+            MessageBox.Show(Output(Ucenici[BrojDn].UpdateUcenikData(tx, OrigData, UserKlas._token)));
+
+            SortUcenici();
+        }
+
+        public string ProektniToString(List<string> tx)
+        {
+            int i = 0;
+            string s = "";
+            foreach (string x in tx)
+            {
+                if (i % 2 == 0)
+                {
+                    s += x + ",";
+                }
+                else s += x + ";";
+                i++;
+            }
+            s = s.Substring(0, s.Length - 1);
+            return s;
         }
 
         private string Output(string answer)
@@ -368,18 +347,17 @@ namespace Frontend
 
         private void CreateUcenikImgClicked(object sender, MouseButtonEventArgs e)
         {
-            if (!UserKlas._p._smerovi.ContainsKey(Answer[3].Text))
+            if (ListPolinjaCB.Find(x => x.Tag.ToString() == "Смер").Items.Count == 0)
             {
                 int num = (int)MessageBox.Show("Смерот не се совпаѓа");
             }
             else
             {
-                CreateUcenik(Answer[0].Text, Answer[2].Text, Answer[1].Text, Answer[3].Text, (BrojDn + 1).ToString());
+                CreateUcenik(polinja[0].Odgovor, polinja[2].Odgovor, polinja[1].Odgovor, polinja[3].Odgovor, (BrojDn + 1).ToString());
                 int bk = BrojDn;
                 BrojDn = Ucenici.Count - 1;
-                Save();
+                Save2();
                 BrojDn = bk;
-                SortUcenici();
             }
         }
 
@@ -399,11 +377,7 @@ namespace Frontend
         
         private void SortUcenici()
         {
-            // var ordered = Ucenici.OrderBy(x => x._broj).ThenBy(x => x._duplicate_ctr);
-            
-            CultureInfo culture = new CultureInfo("mk-MK");
-            StringComparer strcomparer = StringComparer.Create(culture, true);
-            var ordered = Ucenici.OrderBy(x => x._prezime, strcomparer).ThenBy(x => x._ime, strcomparer).ThenBy(x => x._duplicate_ctr);
+            var ordered = Ucenici.OrderBy(x => x._broj);
 
             Ucenici = ordered.ToList();
             Home_Page.ucenici = Ucenici;
