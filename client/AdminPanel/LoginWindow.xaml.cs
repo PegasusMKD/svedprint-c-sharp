295ea5c146a569e5c300f9e0a1328ad96a4e910b
dsekerov <darijan2002@gmail.com>
2019-07-20 01:58:03 +0200

    finish binding to codebehind

--- client/AdminPanel/LoginWindow.xaml.cs
+++ client/AdminPanel/LoginWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,11 +20,48 @@ namespace AdminPanel
     /// <summary>
     /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class LoginWindow : INotifyPropertyChanged
     {
+        private string _username, _password;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                if(value != _username)
+                {
+                    _username = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (value != _password)
+                {
+                    _password = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
         public LoginWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"{Username} - {Password}");
         }
     }
 }
