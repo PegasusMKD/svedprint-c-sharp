659e288286010884b4bdca332044802fb224a088
PegasusMKD <csprogrammers@outlook.com>
2019-10-06 08:41:57 +0200

    Changed the ServerURI in Resources from localhost to the server's URL

--- client/AdminPanel/Properties/Resources.Designer.cs
+++ client/AdminPanel/Properties/Resources.Designer.cs
@@ -97,7 +97,7 @@ namespace AdminPanel.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://127.0.0.1:8000/admin_stable/.
+        ///   Looks up a localized string similar to https://ocenki.herokuapp.com/admin_stable/.
         /// </summary>
         public static string ServerURI {
             get {
