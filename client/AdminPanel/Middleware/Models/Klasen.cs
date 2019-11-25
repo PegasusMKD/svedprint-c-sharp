bc1669916f122ada5953e15f3156a7e3aef843c2
Luka Jovanovski <luka.jovanovski@hotmail.com>
2019-11-25 04:35:11 +0100

    Profesori Fixed

--- client/AdminPanel/Middleware/Models/Klasen.cs
+++ client/AdminPanel/Middleware/Models/Klasen.cs
@@ -93,15 +93,15 @@ namespace AdminPanel.Middleware.Models
         }
 
         public List<Pole> Polinja;
-        public List<Pole> GetPolinja()
+        public List<Pole> GetPolinja(Dictionary<string, List<Klasen>> users)
         {
 
            Polinja = new List<Pole>
                     {
-                        new Pole ("Корисничко име" , new string[] { Username } , "UsernameBind"),
+                        new Pole ("Корисничко име" , new string[] { Username } , "UsernameBind" , users),
                         new Pole ("Лозинка" ,  new string[] { "password" } , "" , "PW"),
-                        new Pole ("Име" , new string[] { Ime } , "Ime"  ),
-                        new Pole ("Презиме" , new string[] { PrezimeBind } , "PrezimeBind"  ),
+                        new Pole ("Име" , new string[] { Ime } , "Ime"  , users),
+                        new Pole ("Презиме" , new string[] { PrezimeBind } , "PrezimeBind"  ,users ),
                         //new Pole ("PMA" , new string[] { "mat" , "mak" , "ger" , "asdf" , "fdas" , "dfass"  } , "parametar" , "Predmeti" , "dfass" ),
                     };
             return Polinja;
