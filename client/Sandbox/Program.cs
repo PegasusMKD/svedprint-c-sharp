6f8cffc61ab8a0b640ea74a81c80809ee3996ac4
Darijan Sekerov <darijan2002@gmail.com>
2020-05-22 14:55:28 +0200

    sandbox project

---
+++ client/Sandbox/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string json = "{" +
+                " \"companies\": {" +
+                    "\"abc\": \"Abc Company\", " +
+                    "\"def\": \"Def Company\", " +
+                    "\"ghi\": { " +
+                        "\"abc\": 4, " +
+                        "\"ee\": \"fof\"" +
+                    " }" +
+                "}," +
+                "\"name\" : \"darijan\"" +
+            "}";
+            JObject root = JObject.Parse(json);
+            Console.WriteLine(root.Value<JObject>("companies").Property("abc")); 
+        }
+
+    }
+}
