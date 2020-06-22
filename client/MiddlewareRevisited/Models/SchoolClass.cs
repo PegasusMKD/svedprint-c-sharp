31698c63d978c14415b27b6bc0b41291f045b105
Darijan Sekerov <darijan2002@gmail.com>
2020-06-22 02:31:02 +0200

    school class

--- client/MiddlewareRevisited/Models/SchoolClass.cs
+++ client/MiddlewareRevisited/Models/SchoolClass.cs
@@ -1,6 +1,20 @@
-﻿namespace MiddlewareRevisited.Models
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MiddlewareRevisited.Models
 {
     public class SchoolClass
     {
+        [JsonProperty]
+        public string id;
+        [JsonProperty]
+        public string name;
+        [JsonProperty]
+        public Year year;
+        [JsonProperty]
+        public List<Student> students;
+        [JsonProperty]
+        public List<SubjectOrientation> subjectOrientations;
+
     }
 }
