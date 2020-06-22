ed0bb61f60a5be52bdcb66b48fef7c3969219fd0
Darijan Sekerov <darijan2002@gmail.com>
2020-06-22 02:42:52 +0200

    year class

--- client/MiddlewareRevisited/Models/Year.cs
+++ client/MiddlewareRevisited/Models/Year.cs
@@ -1,6 +1,15 @@
-﻿namespace MiddlewareRevisited.Models
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MiddlewareRevisited.Models
 {
     public class Year
     {
+        [JsonProperty]
+        public string id;
+        [JsonProperty]
+        public DateTime dateWhenTestimonyConfirmed;
+        [JsonProperty]
+        public string name;
     }
 }
